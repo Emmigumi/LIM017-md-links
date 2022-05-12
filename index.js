cdb6c3a61b5d51e9ba0c47ced96fbe36471a8176
Emmigumi <20160133@lamolina.edu.pe>
2022-05-11 23:10:16 -0500

    try fall

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = () => {
+
+/* module.exports = () => {
   // ...
-};
+}; */
+
+ const file = () => {require('file-system')};
+ const fs = () => {require('fs');
+
+console.log('UNO', file);
+console.log('DOS', fs);
