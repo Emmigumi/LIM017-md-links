63b1fd9f4c0a755382cbdd53ee7fa3df8b24f6dc
Emmigumi <20160133@lamolina.edu.pe>
2022-05-25 18:00:45 -0500

    traverse the directory and list the files in an array

--- src/util.js
+++ src/util.js
@@ -4,25 +4,36 @@ import fs from 'fs';
 // 1. Convertir una ruta relativa en absoluta
 // isAbsolute: Verifica si la ruta es absoluta // resolve: Convierte una ruta relativa en una absoluta
 export const transformPathAbsolute = argPath => path.isAbsolute(argPath) ? argPath : path.resolve(argPath);
+// console.log(transformPathAbsolute(process.argv[2]));
 // 2. Verficar la existencia de la ruta
 export const verifyPathExist = (argPath) => fs.existsSync(transformPathAbsolute(argPath));
+// console.log(transformPathAbsolute(process.argv[2]));
 // 3. Verificar que sea archivo
 export const verifyIsFile = (argPath) => fs.lstatSync(argPath).isFile();
 // 4. Verificar que sea directorio
 export const verifyIsDirectory = (argPath) => fs.lstatSync(argPath).isDirectory();
+// console.log(verifyIsDirectory(process.argv[2]));
 // 5. Identifiar la extensión de la ruta
 export const recognizePathExtension = (argPath) => path.extname(argPath);
-// 6. Permite leer el contenido de un directorio retornado en un array
-export const arrayListFile = (argPath) => {
-	let arrayList = fs.readdirSync(argPath);
-	if(arrayList.length > 0){
-		arrayList= arrayList.map((e) => path.join(argPath, e));
-	}
-	return arrayList;
-};
-
-// console.log(arrayListFile(process.argv[2]));
+// 6. Permite leer los archivos de un directorio retornadolos en un array
+const readDirectory = (argPath) => fs.readdirSync(argPath);
 // 7. Recorrer el directorio
+export const arrayListFile = (argPath) => {
+	 let arrayList = [];
+	if(verifyIsDirectory(argPath) === false){
+		arrayList.push(argPath);
+	}
+
+    else {
+        readDirectory(argPath).forEach(file => {
+           const filePath = path.join(argPath, file);
+           arrayList = arrayList.concat(arrayListFile(filePath))
+            });
+            }
+        return arrayList;
+}
+console.log(arrayListFile(process.argv[2]));
+
 
 // 8. Identificar la extención de tipo .md
 //
@@ -76,4 +87,22 @@ console.log(fileContentShow(process.argv[2])); */
   });
 }
 verifyPathExist(process.argv[2]); */
+
+/* TRY3 RECORRER DIRECTORIO */
+/* function recursivelyDelete(filePath) {
+    // check if directory or file
+    const stats = fs.statSync(filePath);
+    // if file unlinkSync
+    if (stats.isFile()) {
+     fs.readdirSync(filePath);
+    }
+    // if directory, readdir and call recursivelyDelete for each file
+    else {
+     const files = fs.readdirSync(filePath);
+     files.forEach((file) => {
+      recursivelyDelete(path.join(filePath, file));
+     });
+     fs.rmdirSync(filePath);
+    }
+   } */
 
