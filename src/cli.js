6b07b3f14741ee732ad8fcbfecee01352b9579f2
Emmigumi <20160133@lamolina.edu.pe>
2022-06-08 18:08:21 -0500

    complete tests

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable no-unused-expressions */
 import {mdLinks} from '../src/index.js';
+import chalk from 'chalk';
 
 const inputsTerminal = process.argv;
 const inputArgPath = inputsTerminal[2];
@@ -10,34 +11,34 @@ if(inputArgPath) {
     if(inputsTerminal.length === 3){
         mdLinks(inputArgPath, { validate: false, stats: false })
         .then((res)=>{res.forEach((e)=>{
-            const answerCli = `${e.file} ${e.href} ${e.text}`;
-        return console.log(answerCli);
+            const answerCli = `${e.file} /n${chalk.blue(e.href)} /n${e.text}`;
+         return answerCli;
     })})
     .catch((err)=>console.log(err));
     } else if (inputsTerminal.length === 4){
         if (options.includes('--validate')){
             mdLinks(inputArgPath, { validate: true, stats: false })
             .then((res)=>{res.forEach((e)=>{
-                const answerCli = `${e.file} ${e.href} ${e.message} ${e.status}  ${e.text}`;
-            return console.log(answerCli);
+              const answerCli = console.log(`${e.file} /n${chalk.blue(e.href)} /n${chalk.yellow(e.message)} /n${chalk.magenta(e.status)}  /n${e.text}`);
+            return answerCli;
         })})
         .catch((err)=>console.log(err));
         } else if (inputsTerminal.length === 4){
             if (options.includes('--stats')){
                 mdLinks(inputArgPath, { validate: false, stats: true })
-                .then((res)=>console.log(res))
-                .catch((err)=> console.log(err));
+                .then((res)=>console.log(chalk.blueBright(res)))
+                .catch((err)=> console.log(chalk.red(err)));
             }
         } else {
-            console.log('Error: La opción ingresada no es válida. Opciones permitidas: --validate o --stats');
+            console.log(chalk.red('Error: La opción ingresada no es válida. Opciones permitidas: --validate o --stats'));
         }
     } else if(inputsTerminal.length === 5 && options.includes('--validate') && options.includes('--stats')){
         mdLinks(inputArgPath, { validate: true, stats: true })
-        .then((res)=>console.log(res))
-        .catch((err)=> console.log(err));
+        .then((res)=>console.log(chalk.yellow(res)))
+        .catch((err)=> console.log(chalk.red(err)));
     } else {
-        console.log('Error: La opción ingresada no es válida. Opciones permitidas: --validate o --stats');
+        console.log(chalk.red('Error: La opción ingresada no es válida. Opciones permitidas: --validate o --stats'));
     }
 } else {
-    console.log('Error: Debe ingresar una ruta. Vuelva a intentarlo');
+    console.log(chalk.red('Error: Debe ingresar una ruta. Vuelva a intentarlo'));
 }
